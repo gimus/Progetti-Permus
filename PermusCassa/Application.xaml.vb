18e43b0014ec6b60d33a7661b796ee31a2e7e900
davegimo <davegimo@gmail.com>
2019-04-22 19:19:24 +0200

    nuovo progetto

---
+++ PermusCassa/Application.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Globalization
+Imports System.Threading
+Imports System.Windows.Markup
+Imports Permus
+Class Application
+
+    Public Shared C As ClientBlockChain
+
+    Public Shared Property mw As MainWindow
+
+
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+        Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("it-IT")
+        Thread.CurrentThread.CurrentUICulture = New System.Globalization.CultureInfo("it-IT")
+        FrameworkElement.LanguageProperty.OverrideMetadata(GetType(FrameworkElement), New FrameworkPropertyMetadata(
+        XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)))
+        C = New ClientBlockChain(My.Settings.BlockMasterPollingTimerIntervalSeconds, localStoreTypeEnum.xml_plain)
+    End Sub
+
+
+End Class
